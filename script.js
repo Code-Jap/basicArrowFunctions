536edfccb2954bd0e97b4b9af3b0b74fec6ab591
Jasper <80272820+Code-Jap@users.noreply.github.com>
2021-06-17 14:29:13 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+/*
+
+const ikRockArrowFunctions = () => {
+    console.log('Joe, ik rock de arrow functions!')
+};
+
+ikRockArrowFunctions();
+
+*/
+
+const fivePlusSeven = () => 5 + 7
+
+console.log(fivePlusSeven());
+
+
+
+let myFunction = (a, b) => a + b
+
+console.log(myFunction(3, 2));
+
+
+
+let addFive = a => a + 5
+
+console.log(addFive(4));
+
+
+
+let createObject  = () => ({greeting: 'Hoi'})
+
+console.log(createObject());
